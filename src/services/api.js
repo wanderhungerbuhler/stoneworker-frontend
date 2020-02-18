7926aacbb1f6d2b93c6f52edc490ec49556b8913
Wander de Souza Hungerbuhler Filho <wfilho@cge.rj>
2020-02-18 17:22:28 -0300

    Update URI API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://stoneworkerapi.herokuapp.com/api"
+  baseURL: "https://stoneworker-api.herokuapp.com/api"
 });
 
 export default api;
