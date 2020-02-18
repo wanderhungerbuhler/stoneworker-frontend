ce08714108332e67d796604b2484543d05edb39a
Evandro Ribeiro <ribeiroevandro@live.com>
2020-02-18 10:56:50 -0300

    Fix in API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://stoneworkerapi.herokuapp.com/api',
+  baseURL: "https://stoneworker-api.herokuapp.com/api"
 });
 
 export default api;
